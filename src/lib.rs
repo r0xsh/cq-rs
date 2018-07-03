fbdef5a1c4b58c1b8234a72241235c86f600e53e
Antoine BAGNAUD <bagnaud.antoine@gmail.com>
2018-07-04 01:06:39 +0200

    quack

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,57 @@
+pub trait Event {}
+pub struct Message;
+
+#[derive(Debug)]
+pub struct MessageEventData {
+    pub message: String
+}
+
+#[derive(Debug)]
+pub enum MessageEvent {
+    Quacked(MessageEventData)
+}
+
+impl PartialEq for MessageEvent {
+    fn eq(&self, other: &MessageEvent) -> bool {
+        match (self, other) {
+            (MessageEvent::Quacked(a), MessageEvent::Quacked(b)) => {
+                return a == b;
+            }
+        }
+    }
+}
+
+
+impl PartialEq for MessageEventData {
+    fn eq(&self, other: &MessageEventData) -> bool {
+        self.message == other.message
+    }
+}
+
+impl Message {
+    pub fn quack(message: &str) -> MessageEvent {
+        MessageEvent::Quacked(MessageEventData {
+            message: message.to_owned()
+        })
+    }
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn raise_message() {
+        let mut history = vec!();
+        history.push(Message::quack("coucou"));
+
+        assert_eq!(
+            history[0],
+            MessageEvent::Quacked(MessageEventData{
+                message: "coucou".to_string()
+            })
+        )
     }
 }
