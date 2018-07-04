43c0246a92238d976b226dbd42600af3a992e4ec
Antoine BAGNAUD <bagnaud.antoine@gmail.com>
2018-07-04 02:37:06 +0200

    buisness logic

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,23 @@ pub trait Event {}
 pub struct Message {
     pub uuid: Option<Uuid>,
     pub deleted: bool,
-    pub message: String
+    pub message: String,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct MessageEventData {
     pub uuid: Uuid,
-    pub message: String
+    pub message: String,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum MessageEvent {
     Quacked(MessageEventData),
-    Deleted
+    Deleted,
 }
 
 impl Message {
-
     pub fn new(events: &Vec<MessageEvent>) -> Message {
-
         let mut msg = Message::default();
 
         for event in events {
@@ -36,70 +34,81 @@ impl Message {
 
     pub fn apply(&mut self, event: &MessageEvent) {
         match event {
-                MessageEvent::Quacked(e) => {
-                    self.uuid = Some(e.uuid);
-                    self.message = e.message.to_owned()
-                },
-                MessageEvent::Deleted => {
-                    self.deleted = true;
-                }
+            MessageEvent::Quacked(e) => {
+                self.uuid = Some(e.uuid);
+                self.message = e.message.to_owned()
             }
+            MessageEvent::Deleted => {
+                self.deleted = true;
+            }
+        }
     }
 
     pub fn default() -> Message {
         Message {
             uuid: None,
             deleted: false,
-            message: String::new()
+            message: String::new(),
         }
     }
 
     pub fn quack(events: &mut Vec<MessageEvent>, uuid: Uuid, message: &str) {
         events.push(MessageEvent::Quacked(MessageEventData {
             uuid: uuid,
-            message: message.to_owned()
+            message: message.to_owned(),
         }));
     }
 
-    pub fn delete(&mut self, events: &mut Vec<MessageEvent>){
-        let event = MessageEvent::Deleted;
-        self.apply(&event);
-        events.push(event);
+    pub fn delete(&mut self, events: &mut Vec<MessageEvent>) {
+        if !self.deleted {
+            let event = MessageEvent::Deleted;
+            self.apply(&event);
+            events.push(event);
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn message_quacked() {
-
+    fn raise_message_quacked_when_quack_message() {
         let uuid = Uuid::new_v4();
-        let mut history = vec!();
+        let mut history = vec![];
         Message::quack(&mut history, uuid, "coucou");
-
 
         assert_eq!(
             history[0],
-            MessageEvent::Quacked(MessageEventData{
+            MessageEvent::Quacked(MessageEventData {
                 uuid: uuid,
                 message: "coucou".to_string()
             })
         )
     }
 
-
     #[test]
-    fn message_deleted() {
-
-        let mut history = vec!();
+    fn raise_message_deleted_when_delete_message() {
+        let mut history = vec![];
         Message::quack(&mut history, Uuid::new_v4(), "coucou");
 
         let mut message = Message::new(&history);
         message.delete(&mut history);
 
         assert_eq!(*history.last().unwrap(), MessageEvent::Deleted);
+        assert_eq!(history.len(), 2);
+    }
+
+    #[test]
+    fn not_raise_message_deleted_when_delete_deleted_message() {
+        let mut history = vec![];
+        Message::quack(&mut history, Uuid::new_v4(), "coucou");
+
+        let mut message = Message::new(&history);
+        message.delete(&mut history);
+        message.delete(&mut history);
+
+        assert_eq!(history.len(), 2);
     }
 
 }
